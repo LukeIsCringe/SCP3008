241e72ed32f54fa1635f84fde2bd6ee902858ad9
LukeIsCringe <lukejwatts05@gmail.com>
2022-06-18 03:46:42 +0100

    Revert "Crouch working"
    This reverts commit b461acdc3a114506650811794fb63af2d9cbc2a9.

--- Ikea game/Assets/characterMovement.cs
+++ Ikea game/Assets/characterMovement.cs
@@ -4,6 +4,7 @@ public class characterMovement : MonoBehaviour
 {
     public CharacterController controller;
 
+
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
@@ -46,22 +47,11 @@ public class characterMovement : MonoBehaviour
         {
             speed = 18f;
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
+        else
         {
             speed = 12f;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            gameObject.GetComponent<Transform>().transform.localScale = new Vector3(1, 0.6f, 1);
-            gameObject.GetComponent<Transform>().transform.position = transform.position - new Vector3(0, 0.4f, 0);
-            speed = 6f;
-        }
-        if (Input.GetKeyUp(KeyCode.LeftControl))
-        {
-            gameObject.GetComponent<Transform>().transform.localScale = new Vector3(1, 1, 1);
-            gameObject.GetComponent<Transform>().transform.position = transform.position + new Vector3(0, 0.4f, 0);
-            speed = 12f;
-        }
+
     }
 }
